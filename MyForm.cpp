25484e50ce52464d90b5616f5408568cbb8b8f2d
lyrandle <79120811+lyrandle@users.noreply.github.com>
2021-09-17 01:02:47 -0500

    Add files via upload
    Sprint 0

---
+++ MyForm.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	RandomNumberGenerator::MyForm form;
+	Application::Run(% form);
+}
